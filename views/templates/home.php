75adb64bc07b59f2ec709292df00ab2980f03550
NathSourire <134375430+NathSourire@users.noreply.github.com>
2023-10-18 22:23:59 +0200

    1

--- views/templates/home.php
+++ views/templates/home.php
@@ -1,28 +1,24 @@
 <h1>Accueil</h1>
 
-<div class="gallery">
-    <div class="row row-cols-1 row-cols-md-4 gap-5 d-flex justify-content-center my-5">
-
-        <?php
-        foreach ($vehicles as $vehicle) {
-        ?>
-            <div class="card col">
-            <?php if ($vehicle->picture){ ?>
-        <img class="img-fluid" src="/public/uploads/vehicles/<?= $vehicle->picture ?>" alt="">
-        <?php }else{ ?>
-        <img class="img-fluid" src="/public/assets/img/pokemon97.webp" alt="">
-        <?php } ?>  
-        <!-- <img class="img-fluid" src="/public/uploads/vehicles/<?= ($vehicle->picture == true) ? $vehicle->picture : '/public/assets/img/pokemon97.webp' ?>" alt=""> -->
-                <h5 class="card-title my-3">Card title</h5>
-                <li><?= 'Catégorie : ' . $vehicle->type  ?></li>
-                <li><?= 'Marque : ' . $vehicle->brand ?></li>
-                <li><?= 'Model : ' . $vehicle->model ?></li>
-                <li><?= 'Immatriculation : ' . $vehicle->registration ?></li>
-                <li><?= 'Kilométrage : ' . $vehicle->mileage ?></li> 
-            </div>
-            <br>
-        <?php } ?>
-
-
-    </div>
+<div class="row row-cols-1 row-cols-md-4 gap-5 d-flex justify-content-center my-5">
+    <?php
+    foreach ($vehicles as $vehicle) {
+    ?>
+        <div class="card border-3 border-warning bg-primary col">
+            <?php if ($vehicle->picture) { ?>
+                <img class="img-fluid card-img-top" src="/public/uploads/vehicles/<?= $vehicle->picture ?>" alt="">
+            <?php } else { ?>
+                <img class="img-fluid card-img-top" src="/public/assets/img/pokemon97.webp" alt="">
+            <?php } ?>
+            <!-- <img class="img-fluid" src="/public/uploads/vehicles/<?= ($vehicle->picture == true) ? $vehicle->picture : '/public/assets/img/pokemon97.webp' ?>" alt=""> -->
+            <h5 class="card-title my-3"><?= $vehicle->id_vehicles ?></h5>
+            <li class="card-text"><?= 'Catégorie : ' . $vehicle->type  ?></li>
+            <li class="card-text"><?= 'Marque : ' . $vehicle->brand ?></li>
+            <li class="card-text"><?= 'Model : ' . $vehicle->model ?></li>
+            <li class="card-text"><?= 'Immatriculation : ' . $vehicle->registration ?></li>
+            <li class="card-text"><?= 'Kilométrage : ' . $vehicle->mileage ?></li>
+        </div>
+        <br>
+    <?php } ?>
+</div>
 </div>
